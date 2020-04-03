79add28a41a278cc2f3c69df2f97a6886059f7c1
jamariod <j25ghs99@aol.com>
2020-04-03 10:37:06 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,15 +3,19 @@ Write a program that prints the numbers from 1 to 'n'.
 But for multiples of three print 'Fizz' instead of the number and for the multiples of five print 'Buzz'. 
 For numbers which are multiples of both three and five print 'FizzBuzz'*/
 
-function fizzbuzzTest() {
-  for (i = 0; i <= 100; i++) {
-    if (i % 15 == 0) {
-      console.log("FizzBuzz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
+function fizzbuzzTest(n) {
+  
+  for (let i = 0; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 == 0) {
+      console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(i);
     }
   }
 }
-fizzbuzzTest();
+
+fizzbuzzTest(100);
